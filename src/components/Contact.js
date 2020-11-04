ff98d0276c487fa77ee1373471fc33624dd84525
Guillaume <guillaumec.dev@gmail.com>
2020-11-04 22:06:09 +0100

    first commit

---
+++ src/components/Contact.js
@@ -0,0 +1,19 @@
+import './Contact.css'
+
+export function Contact() {
+    const avatarUrl= 'https://randomuser.me/api/portraits/men/45.jpg';
+    const name = 'Micheal Chavez';
+    return (
+        <div className='Contact'>
+            <img alt='profile picture' className='avatar' srcSet={avatarUrl}/>
+            <div>
+                <h4 className='name'>{name}</h4>
+                <div className='status'>
+                    <span className='status-online'/>
+                    <p className='status-text'>online</p>
+                </div>
+
+            </div>
+        </div>
+    );
+};
